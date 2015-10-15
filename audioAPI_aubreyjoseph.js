65228f8bf09cc6af549ae2550533e6d8e9bf27b2
theawesomerazz <awesomerazz1.0@gmail.com>
2015-10-15 14:51:32 -0400

    Create audioAPI_aubreyjoseph.js

---
+++ audioAPI_aubreyjoseph.js
@@ -0,0 +1,8 @@
+var a = document.getElementById("audioAPI");
+var aCtx = a.getContext("2d");
+var imag = new Image();
+var whateveruwannanameit = new Audio("leavingearth.mp3");
+whateveruwannanameit.loop = true
+imag.src = "world.jpg";
+imag.onload = function () {aCtx.drawImage(imag,0,0, 500, 500);};
+whateveruwannanameit.play();
