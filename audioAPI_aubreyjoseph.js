ea27654f2a93fe80c0ecbf7f81bc78fbb2d41e8b
theawesomerazz <awesomerazz1.0@gmail.com>
2015-10-20 13:50:01 -0400

    Update audioAPI_aubreyjoseph.js

--- audioAPI_aubreyjoseph.js
+++ audioAPI_aubreyjoseph.js
@@ -3,6 +3,6 @@ var aCtx = a.getContext("2d");
 var imag = new Image();
 var whateveruwannanameit = new Audio("leavingearth.mp3");
 whateveruwannanameit.loop = true
-imag.src = "http://i58.tinypic.com/2d0l3j8.jpg";
+imag.src = "http://oi58.tinypic.com/2d0l3j8.jpg";
 imag.onload = function () {aCtx.drawImage(imag,0,0, 500, 500);};
 whateveruwannanameit.play();
